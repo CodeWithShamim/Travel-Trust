3bd936805f89c6b177139e49b3ec6477b09f08ff
codewithshamim <shamimislamonline@gmail.com>
2023-11-24 23:08:16 +0600

    updated

--- travel-trust-frontend/src/views/message/index.tsx
+++ travel-trust-frontend/src/views/message/index.tsx
@@ -1,13 +1,19 @@
 "use client";
 
-import { Button, Input, Layout } from "antd";
-import React from "react";
+import { Button, Input, Layout, Modal } from "antd";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 const { Sider, Content } = Layout;
 import { CiMenuKebab } from "react-icons/ci";
 
 const { TextArea } = Input;
 
 const Message = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
   return (
     <div className="max-w-[1200px] mx-auto px-4 relative">
       <Layout hasSider className="bg-white">
@@ -24,6 +30,15 @@ const Message = () => {
             ))}
           </div>
         </Sider>
+
+        <Modal open={isOpen} title="Comming soon..." footer={null}>
+          <p className="pb-4 text-red-500">
+            Work in progress. Stay tuned for updates!
+          </p>
+          <Link href={"/"}>
+            <Button type="primary">Go Home</Button>
+          </Link>
+        </Modal>
 
         <Content className="relative">
           <div className="absolute bottom-4 left-2 w-full flex items-center gap-3">

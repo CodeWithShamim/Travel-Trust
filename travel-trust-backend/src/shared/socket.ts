6e287fa108eee54fc04dcf70661f3a6deee9b44b
codewithshamim <shamimislamonline@gmail.com>
2023-12-01 21:01:51 +0600

    u

--- travel-trust-backend/src/shared/socket.ts
+++ travel-trust-backend/src/shared/socket.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-unused-vars */
 import http from 'http';
 import { Server } from 'socket.io';
 import app from '../app';
@@ -7,11 +5,13 @@ import app from '../app';
 export const newServer = http.createServer(app);
 
 const io = new Server(newServer, {
-  cors: {},
+  cors: {
+    origin: '*',
+  },
 });
 
 io.on('connection', socket => {
-  //   console.log('User connected:', socket.id);
+  console.log('User connected:', socket.id);
 });
 
 export default io;
